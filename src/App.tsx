ddb449905929b559f0519ccf7252a9f70fb32e69
William Wu <williamwu@Williams-MacBook-Air-3.local>
2022-12-19 20:23:52 -0600

    Added education

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,12 @@ function App() {
       </div>
 
       <div className="App-div">
+        <header className="App-header">Education</header>
+        <h3 className="App-h3">University of Illinois at Urbana-Champaign</h3>
+        <p className="App-p">B.S. in Mathematics and Computer Science</p>
+      </div>
+
+      <div className="App-div">
         <header className="App-header">Work Experience</header>
 
         <div>
@@ -34,11 +40,6 @@ function App() {
             collected performance metrics
           </p>
         </div>
-      </div>
-
-      <div className="App-div">
-        <header className="App-header">Projects</header>
-        <h3 className="App-h3">WIP</h3>
       </div>
     </>
   );
